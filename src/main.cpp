2064546e8b6d0a5893a685cbbecf9e31b0282972
savanto <savanto@gmail.com>
2014-01-21 22:24:27 -0800

    Add valid merkle root

--- src/main.cpp
+++ src/main.cpp
@@ -2784,7 +2784,7 @@ bool InitBlockIndex() {
         printf("%s\n", hash.ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x0"));
+        assert(block.hashMerkleRoot == uint256("0x00039cfd8fe6288515b3817a2d8f140391d3bf834170ebb626ad0af408c2e617"));
         block.print();
         assert(hash == hashGenesisBlock);
 
